dafbefba084ca94a1f9fb4afc7d76cc483c78869
Wojciech Migda <wojtek.golf@interia.pl>
2022-08-18 23:31:50 +0200

    Add legacy zfec benchmark helper script
    Addresses (#66)

---
+++ bench/tools/legacy_zfec.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -e
+
+################################################################################
+###
+### This script will download legacy zfec code and build modified bench_zfex
+### tool with different sets of compiler flags. Then, built executable will be
+### run with different arguments to measure legacy algorithm's performance.
+###
+################################################################################
+
+
+# cheat sheet: https://en.wikichip.org/wiki/intel/cpuid
+lscpu
+
+git clone --depth 1 https://github.com/tahoe-lafs/zfec
+ln -sf fec.h zfec/zfec/zfex.h && touch zfec/zfec/zfex_macros.h zfec/zfec/zfex_pp.h
+
+wget https://raw.githubusercontent.com/WojciechMigda/zfex/main/bench/bench_zfex.c -O zfec/bench/bench_zfec.c
+
+cat << 'EOF' > zfec/bench/Makefile
+STRIDE := 8192
+UNROLL := 16
+
+bench_zfec: bench_zfec.c ../zfec/fec.c ../zfec/fec.h
+	${CC} ${CFLAGS} -fno-strict-aliasing -Wall -Werror -Wshadow -Wdate-time -Wformat -Werror=format-security -std=c99 -DSTRIDE=$(STRIDE) -DZFEX_STRIDE=$(STRIDE) -DUNROLL=$(UNROLL) -o bench_zfec bench_zfec.c ../zfec/fec.c  -I../zfec
+
+clean:
+	rm bench_zfec > /dev/null 2>&1 || true
+EOF
+
+
+build() {
+
+make -C zfec/bench clean
+
+CFLAGS="${EXTRA_CFLAGS} -DZFEX_SIMD_ALIGNMENT=16 -DZFEX_INTEL_SSSE3_FEATURE=0 -DZFEX_ARM_NEON_FEATURE=0 -DZFEX_INLINE_ADDMUL_FEATURE=0 -DZFEX_INLINE_ADDMUL_SIMD_FEATURE=0 -Dfec_encode_simd=fec_encode" \
+  make -C zfec/bench bench_zfec
+
+}
+
+run() {
+
+zfec/bench/bench_zfec ${ARGS} > /dev/null && zfec/bench/bench_zfec ${ARGS}
+
+}
+
+# Benchmark: -O2 and default cpu (pip build scenario)
+EXTRA_CFLAGS="-O2" build
+
+ARGS="-k 7 -m 10 -s 1000000 -i 30" run
+ARGS="-k 223 -m 255 -s 43488 -i 30" run
+
+# Benchmark: -O3 and default cpu
+EXTRA_CFLAGS="-O3" build
+
+ARGS="-k 7 -m 10 -s 1000000 -i 30" run
+ARGS="-k 223 -m 255 -s 43488 -i 30" run
+
+# Benchmark: -O2 and -march=native
+EXTRA_CFLAGS="-O2 -march=native" build
+
+ARGS="-k 7 -m 10 -s 1000000 -i 30" run
+ARGS="-k 223 -m 255 -s 43488 -i 30" run
+
+# Benchmark: -O3 and -march=native
+EXTRA_CFLAGS="-O3 -march=native" build
+
+ARGS="-k 7 -m 10 -s 1000000 -i 30" run
+ARGS="-k 223 -m 255 -s 43488 -i 30" run
